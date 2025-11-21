415072b109ef41766b03cbc030b573ccd69b4229
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 09:53:11 +0700

    membuat variable jenis kelamin

---
+++ app/src/main/java/com/example/myarsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.myarsitekturmvvm.model
+
+import com.example.myarsitekturmvvm.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.lk,
+        R.string.pr
+    )
+}
