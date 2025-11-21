c1ddb06aaeace56c7708936a9f03ad2aeb9891a9
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 10:38:49 +0700

    Menambahkan variable Nama, alamat, gender, dan listdata

--- app/src/main/java/com/example/myarsitekturmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/Formulir.kt
@@ -19,6 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,13 +29,21 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.myarsitekturmvvm.R
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
-){
+    PilihanJK: List<String>,
+    OnSubmitBtnClick : (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+
     Scaffold(
         modifier = Modifier,
         topBar = {
