3711a268a346988376c91d3b6198abf4029e42e5
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 10:25:19 +0700

    Menambahkan fungsi setSiswa untuk mengupdate statusUI (nama, gender, alamat)

--- app/src/main/java/com/example/myarsitekturmvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -5,9 +5,17 @@ import com.example.myarsitekturmvvm.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel(){
     private val  _statusUI = MutableStateFlow(Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls:MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(nama = ls[0], gender = ls[1], alamat = ls[2])
+        }
+
+    }
 
 }
