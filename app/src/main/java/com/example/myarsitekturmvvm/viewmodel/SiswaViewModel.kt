4bed3dfff91989aea66316e1c3b11f8a88fbcd0e
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 10:19:58 +0700

    menambahkan private variable statusUI

--- app/src/main/java/com/example/myarsitekturmvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.myarsitekturmvvm.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.myarsitekturmvvm.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class SiswaViewModel : ViewModel(){
+    private val  _statusUI = MutableStateFlow(Siswa())
+
 }
