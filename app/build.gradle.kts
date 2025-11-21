95000229c8b17abc1bb49b880e38c8d5fa1841a7
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 10:05:54 +0700

    menambahkan implementation lifecycle viewmodel di build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.lifecycle.viewmodel.compose)
+
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
