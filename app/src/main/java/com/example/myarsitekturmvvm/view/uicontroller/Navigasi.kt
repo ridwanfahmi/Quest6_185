60cf003bcd4309206ca80b5f9654dc01dd79cd49
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 09:57:33 +0700

    memindahkan navigasi.kt ke package uicontroller

--- app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -1,4 +1,4 @@
-package com.example.myarsitekturmvvm
+package com.example.myarsitekturmvvm.view.uicontroller
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
