77d746d930707b87dd7c07bd860343870c30a494
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-21 10:21:19 +0700

    menambahkan variable statusUI dan parameter Siswa

--- app/src/main/java/com/example/myarsitekturmvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -3,8 +3,11 @@ package com.example.myarsitekturmvvm.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.myarsitekturmvvm.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SiswaViewModel : ViewModel(){
     private val  _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
 }
